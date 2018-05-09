3421d5d1771fe480ee285ac34bd7d007953f7843
Joseph Abell <joseph.abell@piksel.com>
2018-05-09 14:11:43 +0100

    fix broken test, fix other tests

--- src/buffalo.js
+++ src/buffalo.js
@@ -1,9 +1,9 @@
 const buffalo = (minute) => {
-  if (minute === 30 || minute === 59) {
-    return 'left';
+  if (minute <= 29) {
+    return 'right';
   }
 
-  return 'right';
+  return 'left';
 };
 
 export default buffalo;

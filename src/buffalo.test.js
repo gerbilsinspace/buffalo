7c61a25f0ef3cd51dc1b279ba2fa93a79ec40e9c
Joseph Abell <joseph.abell@piksel.com>
2018-05-09 13:54:53 +0100

    more tweaks to the first test

--- src/buffalo.test.js
+++ src/buffalo.test.js
@@ -3,6 +3,7 @@ import buffalo from './buffalo';
 describe('buffalo', () => {
   it('can run without crashing', () => {
     expect(buffalo).toBeInstanceOf(Function);
+    buffalo();
   });
 
   it('should return "right" when passed the number 0', () => {

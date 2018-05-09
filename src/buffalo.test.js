5ddc4deecbff9c451f07e409f8b51055bafa2b22
Joseph Abell <joseph.abell@piksel.com>
2018-05-09 13:55:24 +0100

    update first test

--- src/buffalo.test.js
+++ src/buffalo.test.js
@@ -2,7 +2,7 @@ import buffalo from './buffalo';
 
 describe('buffalo', () => {
   it('can run without crashing', () => {
-    const result = buffalo()
-    expect(result).toEqual();
+    expect(buffalo).toBeInstanceOf(Function);
+    buffalo();
   });
 });

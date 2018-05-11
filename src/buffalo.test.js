08e5e3e2e6342ccef0fc86926996b8bf89900a63
Joseph Abell <joseph.abell@piksel.com>
2018-05-11 11:07:17 +0100

    update

--- src/buffalo.test.js
+++ src/buffalo.test.js
@@ -2,8 +2,8 @@ import buffalo from './buffalo';
 
 describe('buffalo', () => {
   it('can run without crashing', () => {
+    expect(buffalo).toBeDefined();
     expect(buffalo).toBeInstanceOf(Function);
-    buffalo();
   });
 
   it('should return "right" when passed the number 0', () => {
